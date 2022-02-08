b7da3eed7d8d5a6f2179f47c6fb78ab88d2c0ed3
Siebe Van Nechel <siebe.vannechel@student.odisee.be>
2022-02-08 21:56:23 +0100

    WpfAgenda

---
+++ SlnLes01HerhalingAanvulling/WpfAgenda/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAgenda
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToevoegen_Click(object sender, RoutedEventArgs e)
+        {
+            lblFoutTitel.Visibility = Visibility.Hidden;
+            lblFoutDatum.Visibility = Visibility.Hidden;
+            lblFoutType.Visibility = Visibility.Hidden;
+            lblFoutHerinnering.Visibility = Visibility.Hidden;
+            lblFoutMelding.Visibility = Visibility.Hidden;
+            lblAantalFouten.Content = "";
+            int aantalfouten = 0;
+
+            if (txbTitel.Text=="")
+            {
+                lblFoutTitel.Visibility = Visibility.Visible;
+                aantalfouten++;
+            }
+            if (DatePickerDatum.SelectedDate==null)
+            {
+                lblFoutDatum.Visibility = Visibility.Visible;
+                aantalfouten++;
+            }
+            if(cmbType.SelectedItem==null)
+            {
+                lblFoutType.Visibility = Visibility.Visible;
+                aantalfouten++;
+            }
+            if (RadioGeen.IsChecked==false && Radio1Dag.IsChecked==false && Radio2Dag.IsChecked==false && Radio3Dag.IsChecked==false)
+            {
+                lblFoutHerinnering.Visibility = Visibility.Visible;
+                aantalfouten++;
+            }
+            if (CheckNotificatie.IsChecked==false && CheckEmail.IsChecked==false)
+            {
+                lblFoutMelding.Visibility = Visibility.Visible;
+                aantalfouten++;
+            }
+            else
+            {
+                ListBoxItem afspraak = new ListBoxItem();
+                afspraak.Content = DatePickerDatum.SelectedDate.Value.ToString("dd//mm/yyyy") + " - " + txbTitel.Text;
+                if (cmbType.SelectedIndex==0)
+                {
+                    ListBoxAfspraken.Items.Add(afspraak);
+                }
+                if (cmbType.SelectedIndex == 1)
+                {
+                    ListBoxAfspraken.Items.Add(afspraak);
+                }
+                if (cmbType.SelectedIndex == 2)
+                {
+                    ListBoxAfspraken.Items.Add(afspraak);
+                }
+                //invoer leegmaken
+                txbTitel.Text = "";
+                cmbType.SelectedIndex = -1;
+                Radio1Dag.IsChecked = false;
+                Radio2Dag.IsChecked = false;
+                Radio3Dag.IsChecked = false;
+                RadioGeen.IsChecked = false;
+                CheckEmail.IsChecked = false;
+                CheckNotificatie.IsChecked = false;
+                lblAantalFouten.Visibility = Visibility.Hidden;
+            }
+
+            if (aantalfouten==1)
+            {
+                lblAantalFouten.Content = $"Her formulier bevad {aantalfouten} fout";
+            }
+            else
+            {
+                lblAantalFouten.Content = $"Her formulier bevad {aantalfouten} fouten";
+            }
+        }
+    }
+}
