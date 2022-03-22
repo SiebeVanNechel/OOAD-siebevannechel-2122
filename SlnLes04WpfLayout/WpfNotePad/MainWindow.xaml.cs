58e842a56269c7136af4b4461e9ac968e3aefd32
Siebe Van Nechel <siebe.vannechel@student.odisee.be>
2022-03-22 21:57:55 +0100

    Notepad

--- SlnLes04WpfLayout/WpfNotePad/MainWindow.xaml.cs
+++ SlnLes04WpfLayout/WpfNotePad/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,15 +22,120 @@ namespace WpfNotePad
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string currentFilePath = "";
+        private string initialfolderPath;
         public MainWindow()
         {
             InitializeComponent();
+            initialfolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
             AboutWindow view = new AboutWindow();
             view.Show();
+        }
+
+        private void txtInput_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (txtInput.SelectionLength>0)
+            {
+                btnCopy.IsEnabled = true;
+                btnCut.IsEnabled = true;
+                btnPaste.IsEnabled = false;
+            }
+            else
+            {
+                btnCopy.IsEnabled = false;
+                btnCut.IsEnabled = false;
+                btnPaste.IsEnabled = false;
+            }
+        }
+
+        private void btnCopy_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(txtInput.SelectedText);
+            btnPaste.IsEnabled = true;
+        }
+
+        private void btnCut_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(txtInput.SelectedText);
+            txtInput.Text = "";
+            btnPaste.IsEnabled = true;
+        }
+
+        private void btnPaste_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Paste();
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblChars.Content = "#chars: " + txtInput.Text.Length.ToString();
+
+            if (txtInput.Text == "")
+            {
+                btnSave.IsEnabled = false;
+                btnSaveAs.IsEnabled = false;
+            }
+            else
+            {
+                btnSave.IsEnabled = true;
+                btnSaveAs.IsEnabled = true;
+            }
+        }
+
+        private void btnOpenMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            StreamReader reader;
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = initialfolderPath;
+            if (dialog.ShowDialog() == true)
+            {
+                currentFilePath = dialog.FileName;
+                reader = File.OpenText(currentFilePath);
+                txtInput.Text = reader.ReadToEnd();
+                Tabheader.Header = new DirectoryInfo(dialog.SafeFileName).ToString();
+                reader.Close();
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentFilePath=="")
+            {
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.InitialDirectory = initialfolderPath;
+                if (dialog.ShowDialog()==true)
+                {
+                    currentFilePath = dialog.FileName;
+                }
+                Tabheader.Header = new DirectoryInfo(dialog.SafeFileName).ToString();
+            }
+            StreamWriter writer = File.CreateText(currentFilePath);
+            writer.Write(txtInput.Text);
+            writer.Close();
+        }
+
+        private void btnSaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            StreamWriter writer;
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.InitialDirectory = initialfolderPath;
+            if (dialog.ShowDialog() ==  true)
+            {
+                currentFilePath = dialog.FileName;
+                writer = File.CreateText(currentFilePath);
+                writer.Write(txtInput.Text);
+                Tabheader.Header = new DirectoryInfo(dialog.SafeFileName).ToString();
+                writer.Close();
+            }
+        }
+
+        private void btnExitMenu_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
