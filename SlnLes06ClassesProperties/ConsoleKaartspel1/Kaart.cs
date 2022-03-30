e3ab44c063a58c452735ac2ba3f75e618b861f8d
Siebe Van Nechel <siebe.vannechel@student.odisee.be>
2022-03-30 22:28:02 +0200

    Kaart

---
+++ SlnLes06ClassesProperties/ConsoleKaartspel1/Kaart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleKaartspel1
+{
+    class Kaart
+    {
+        private int _nummer; 
+        public int Nummer
+        {
+            get
+            {
+                return _nummer;
+            }
+            set
+            {
+
+            }
+        }
+    }
+}
