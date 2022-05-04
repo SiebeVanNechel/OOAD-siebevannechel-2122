77735ac68ba3dd53b2a417a073b59a4e7ac76094
Siebe Van Nechel <siebe.vannechel@student.odisee.be>
2022-05-04 23:22:09 +0200

    EscapeGame

---
+++ SlnLes07StaticEnum/WpfEscapeGame/WpfEscapeGame/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfEscapeGame
+{
+    class Item
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsLocked { get; set; } = false;
+        public Item Key { get; set; }
+        public Item HiddenItem { get; set; }
+
+        public bool IsPortable { get; set; } = true;
+        public Item(string name, string desc)
+        {
+            Name = name;
+            Description = desc;
+        }
+
+        public Item(string name, string desc, bool portable)
+        {
+            Name = name;
+            Description = desc;
+            IsPortable = portable;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
