8c760571f472989c059a126ea6e87fb565b9dad5
Siebe Van Nechel <siebe.vannechel@student.odisee.be>
2022-05-04 21:53:12 +0200

    Kassaticket

--- SlnLes07StaticEnum/ConsoleKassaTicket/ConsoleKassaTicket/Program.cs
+++ SlnLes07StaticEnum/ConsoleKassaTicket/ConsoleKassaTicket/Program.cs
@@ -6,12 +6,23 @@ using System.Threading.Tasks;
 
 namespace ConsoleKassaTicket
 {
+    public enum Betaalwijze { Visa, Cash, Bancontact }
     class Program
     {
-        public enum Betaalwijze { Visa, Cash, Bancontact}
         static void Main(string[] args)
         {
-           // Ticket.DrukTicket();
+            Ticket ticket1 = new Ticket(Betaalwijze.Visa, "Annie");
+
+            Product bananen = new Product("bananen" ,(decimal) 1.75 , "P02384" );
+            Product brood = new Product("brood", (decimal)2.10, "P01820");
+            Product kaas = new Product("kaas", (decimal)3.99, "P45612");
+            Product koffie = new Product("koffie", (decimal)4.10, "P98754");
+
+            ticket1.producten.Add(bananen);
+            ticket1.producten.Add(brood);
+            ticket1.producten.Add(kaas);
+            ticket1.producten.Add(koffie);
+            ticket1.DrukTicket();
         }
     }
 }
--- SlnLes07StaticEnum/ConsoleKassaTicket/ConsoleKassaTicket/Ticket.cs
+++ SlnLes07StaticEnum/ConsoleKassaTicket/ConsoleKassaTicket/Ticket.cs
@@ -8,14 +8,42 @@ namespace ConsoleKassaTicket
 {
     class Ticket
     {
+        private string _betaaldMet = "Cash";
         //Properties
-
+        public List<Product> producten { get; set; } = new List<Product>();
+        public Betaalwijze betaaldMet { get; set; }
+        public string kassierster { get; set; }
+        public decimal totaalprijs { get; }
         //Methode
         public void DrukTicket()
         {
-            Console.WriteLine("KASSATICKET");
-            Console.WriteLine("===========");
+            decimal extraKosten = 0;
+            decimal totaal = 0;
+            if (Betaalwijze.Visa==betaaldMet)
+            {
+                extraKosten = (decimal)0.12;
+            }
+            Console.WriteLine($@"KASSATICKET
+===========
+Uw kassier: {kassierster}
+");
+
+            foreach (Product product in producten)
+            {
+                Console.WriteLine(product);
+                totaal += (decimal)product.eenheidsprijs;
+            }
+            Console.WriteLine("\r\n" + "-----------");
+            Console.WriteLine("Visa kosten: " + extraKosten);
+            totaal += extraKosten;
+            Console.WriteLine("Totaal: " + totaal);
+            Console.ReadKey();
         }
         //Constructor
+        public Ticket(Betaalwijze betaaloptie, string kassier)
+        {
+            betaaldMet = betaaloptie;
+            kassierster = kassier;
+        }
     }
 }
