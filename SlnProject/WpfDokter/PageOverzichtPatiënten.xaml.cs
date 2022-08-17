47ac5c87ee0a98db15887f4a69d6c71b41f4cc71
Siebe Van Nechel <siebe.vannechel@student.odisee.be>
2022-08-17 23:44:04 +0200

    XAML

--- SlnProject/WpfDokter/PageOverzichtPatiënten.xaml.cs
+++ SlnProject/WpfDokter/PageOverzichtPatiënten.xaml.cs
@@ -10,25 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfDokter
 {
     /// <summary>
-    /// Interaction logic for LoginWindow.xaml
+    /// Interaction logic for PageOverzichtPatiënten.xaml
     /// </summary>
-    public partial class LoginWindow : Window
+    public partial class PageOverzichtPatiënten : Page
     {
-        public LoginWindow()
+        public PageOverzichtPatiënten()
         {
             InitializeComponent();
-        }
-
-        private void btnLogin_Click(object sender, RoutedEventArgs e)
-        {
-            MainWindow mainWin = new MainWindow();
-            mainWin.Show();
-            this.Close();
         }
     }
 }
